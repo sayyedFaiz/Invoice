2ee9db59072e10b690c1763ffc397ea174fa3488
sayyedFaiz <sayyedfaiz7@gmail.com>
2023-06-03 20:35:03 +0200

    improved

--- public/js/app.js
+++ public/js/app.js
@@ -1,63 +1,67 @@
 var form = document.querySelector(".invoiceForm");
-var inputs = document.querySelectorAll('.form-control')
-var checkboxes = document.querySelectorAll('.form-check-input')
+var inputs = document.querySelectorAll(".form-control");
+var checkboxes = document.querySelectorAll(".form-check-input");
 var addButton = document.querySelector(".btn-add");
-var productTable = document.querySelector('tbody')
-var products = [];
+var productTable = document.querySelector("tbody");
 
-let hasValue = false
+var products = [];
 addButton.addEventListener("click", (e) => {
-  //add a check for empty input fields
-  updateProducts()
+  if (form.checkValidity()) {
+    updateProducts();
+    console.log(products)
+  } else {
+    form.classList.add("was-validated");
+    e.preventDefault();
+    e.stopPropagation();
+
+  }
 });
 
-
-function updateProducts(){
+function updateProducts() {
   const data = Object.fromEntries(new FormData(form).entries());
-  let productQuantity = parseFloat(data.Quantity)
-  let productPrice = parseFloat(data.Price)
-  let productAmount = (productQuantity*productPrice)
-  products.unshift(
-    {'ProductName':data.ProductName,
-    'HSN': data.HSN,
-    'Quantity': productQuantity,
-    'Price': productPrice,
-    'Amount': productAmount
-  })
-  let item =
-`  <tr>
+  let productQuantity = parseFloat(data.Quantity);
+  let productPrice = parseFloat(data.Price);
+  let productAmount = productQuantity * productPrice;
+  products.unshift({
+    ProductName: data.ProductName,
+    HSN: data.HSN,
+    Quantity: productQuantity,
+    Price: productPrice,
+    Amount: productAmount,
+  });
+  let item = `  <tr>
     <td>${products.length}</td>
     <td>${products[0].ProductName}</td>
     <td>${products[0].HSN}</td>
     <td>${productQuantity}</td>
     <td>${productPrice.toFixed(2)}</td>
     <td>${productAmount.toFixed(2)}</td>
-  </tr>`
-  if(products.length <= 10){
-    productTable.insertAdjacentHTML('beforeend',item)
-    calculateTotal()
+  </tr>`;
+  if (products.length <= 10) {
+    productTable.insertAdjacentHTML("beforeend", item);
+    calculateTotal();
   }
-
 }
 
-function calculateTotal(){
-  let grandTotal = 0
-  let roundOff = 0
-  products.forEach((product)=>{
-    grandTotal = grandTotal + product.Amount
-  })
-  checkboxes.forEach((input)=>{
-    if(input.checked){
-      let GST = parseFloat(input.value)
-      document.querySelector(`.${input.name}`).innerHTML = `${GST}%`
-      console.log("Before :",grandTotal)
-      grandTotal = grandTotal + (grandTotal  * GST/100)
-      console.log("After : ",grandTotal)
+function calculateTotal() {
+  let grandTotal = 0;
+  let roundOff = 0;
+  products.forEach((product) => {
+    grandTotal = grandTotal + product.Amount;
+  });
+  checkboxes.forEach((input) => {
+    if (input.checked) {
+      let GST = parseFloat(input.value);
+      document.querySelector(`.${input.name}`).innerHTML = `${GST}%`;
+      console.log("Before :", grandTotal);
+      grandTotal = grandTotal + (grandTotal * GST) / 100;
+      console.log("After : ", grandTotal);
     }
-  })
-  roundOff = (Math.round(grandTotal) - grandTotal).toFixed(2)
-  document.querySelector('.grandTotal').innerHTML  = Math.round(grandTotal).toFixed(2)
-  document.querySelector('.roundOff').innerHTML = roundOff
+  });
+  roundOff = (Math.round(grandTotal) - grandTotal).toFixed(2);
+  document.querySelector(".grandTotal").innerHTML =
+    Math.round(grandTotal).toFixed(2);
+  document.querySelector(".roundOff").innerHTML = roundOff;
 }
 
 
--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const express = require('express')
-const path = require('path');
-const app = express()
-const port = 3000
-app.set('view engine', 'ejs')
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'node_modules')));
+const express = require("express");
+const path = require("path");
+const clientList = require('./public/js/Client.json')
+const app = express();
+const port = 3000;
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "node_modules")));
 
-app.get('/',(req,res)=>{
-  res.render('index')
-})
+app.get("/", (req, res) => {
+  res.render("index", {clientList : clientList.CompanyNames});
+});
+app.get("/print", (req, res) => {
+  res.render("print");
+});
 
 app.listen(port, () => {
-  console.log(` app listening on port ${port}`)
-})
+  console.log(` app listening on port ${port}`);
+});
