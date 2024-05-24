71ea656c65417fd7eea89c3ea3ac5eb46c0bf97e
sayyedFaiz <sayyedfaiz7@gmail.com>
2024-05-24 17:21:34 +0200

    last unknown commit

--- server.js
+++ server.js
@@ -3,9 +3,6 @@ const path = require("path");
 const cors = require("cors");
 const ejs = require("ejs");
 const fs = require("fs");
-const pdf = require("html-pdf");
-const PDFDocument = require("pdfkit");
-const blobStream = require("blob-stream");
 require("dotenv").config();
 const bodyParser = require("body-parser");
 const clientList = require("./public/js/Client.json");
@@ -40,40 +37,17 @@ app.get("/print", (req, res) => {
 });
 
 app.get("/download-invoice", async (req, res) => {
-  // try {
-
-  //   const browser = await puppeteer.launch();
-  //   const page = await browser.newPage();
-  //   // Replace with the full URL of your server when deployed
-  //   await page.goto(`${process.env.SERVER_URL}/print`, {
-  //     waitUntil: "networkidle0",
-  //   });
-  //   const pdf = await page.pdf({
-  //     format: "A4",
-  //     printBackground: true,
-  //     margin: "none",
-  //     preferCSSPageSize: true,
-  //   });
-
-  //   await browser.close();
-
-  //   // Ensure that receivedProducts is defined and has the required properties
-  //   if (receivedProducts && receivedProducts.length > 0) {
-  //     const companyName =
-  //       receivedProducts[receivedProducts.length - 1].Name.trim();
-  //     var fileName = `${companyName}-${date}`;
-  //   }
-
-  //   res.setHeader(
-  //     "Content-Disposition",
-  //     `attachment; filename=${fileName}.pdf`
-  //   );
-  //   res.contentType("application/pdf");
-  //   res.send(pdf);
-  // } catch (error) {
-  //   console.error(error);
-  //   res.status(500).send("Error generating invoice");
-  // }
+  try {
+    //   // Ensure that receivedProducts is defined and has the required properties
+    if (receivedProducts && receivedProducts.length > 0) {
+      const companyName =
+        receivedProducts[receivedProducts.length - 1].Name.trim();
+      var fileName = `${companyName}-${date}`;
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error generating invoice");
+  }
 });
 
 app.listen(port, () => {
