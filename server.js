bc5126be2d2da31e97bd8b95ee9a1a92a1d60d83
sayyedFaiz <sayyedfaiz7@gmail.com>
2024-06-16 17:17:40 +0200

    puppeteer pdf maker

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const cors = require("cors");
 require("dotenv").config();
 const bodyParser = require("body-parser");
-const clientList = require("./public/js/Client.json");
+const clientList = require("./data/Client.json");
 const app = express();
 let receivedProducts = [];
 const date = new Date().toJSON().slice(0, 10);
@@ -17,12 +17,13 @@ app.use(cors());
 
 // Route to serve JSON file
 app.get("/Client.json", (req, res) => {
-  res.sendFile(__dirname + "/public/js/Client.json");
+  res.sendFile(__dirname + "/data/Client.json");
 });
 
 app.get("/", (req, res) => {
-  res.render("index", { clientList: clientList });
+  res.render("index", { clientList });
 });
+
 // Handle the POST request to receive the products data
 app.post("/print", (req, res) => {
   receivedProducts = req.body.products; // Get the products array from the request
@@ -35,42 +36,41 @@ app.get("/print", (req, res) => {
 });
 
 app.get("/download-invoice", async (req, res) => {
-  // try {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    // Replace with the full URL of your server when deployed
+    await page.goto(`${process.env.SERVER_URL}/print`, {
+      waitUntil: "networkidle0",
+    });
+    const pdf = await page.pdf({
+      format: "A4",
+      printBackground: true,
+      margin: "none",
+      preferCSSPageSize: true,
+    });
 
-  //   const browser = await puppeteer.launch();
-  //   const page = await browser.newPage();
-  //   // Replace with the full URL of your server when deployed
-  //   await page.goto(`${process.env.SERVER_URL}/print`, {
-  //     waitUntil: "networkidle0",
-  //   });
-  //   const pdf = await page.pdf({
-  //     format: "A4",
-  //     printBackground: true,
-  //     margin: "none",
-  //     preferCSSPageSize: true,
-  //   });
+    await browser.close();
 
-  //   await browser.close();
+    // Ensure that receivedProducts is defined and has the required properties
+    if (receivedProducts && receivedProducts.length > 0) {
+      const companyName =
+        receivedProducts[receivedProducts.length - 1].Name.trim();
+      var fileName = `${companyName}-${date}`;
+    }
 
-  //   // Ensure that receivedProducts is defined and has the required properties
-  //   if (receivedProducts && receivedProducts.length > 0) {
-  //     const companyName =
-  //       receivedProducts[receivedProducts.length - 1].Name.trim();
-  //     var fileName = `${companyName}-${date}`;
-  //   }
-
-  //   res.setHeader(
-  //     "Content-Disposition",
-  //     `attachment; filename=${fileName}.pdf`
-  //   );
-  //   res.contentType("application/pdf");
-  //   res.send(pdf);
-  // } catch (error) {
-  //   console.error(error);
-  //   res.status(500).send("Error generating invoice");
-  // }
+    res.setHeader(
+      "Content-Disposition",
+      `attachment; filename=${fileName}.pdf`
+    );
+    res.contentType("application/pdf");
+    res.send(pdf);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error generating invoice");
+  }
 });
 
 app.listen(port, () => {
-  console.log(` app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
